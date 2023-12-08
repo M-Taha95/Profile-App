c5b895b4182b64b557b44e7f28da5456502e645a
M-Taha95echo <msoudi2013@gmail.com>
2023-12-08 08:25:26 +0300

    Models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
+from django.utils import timezone
 
-# Create your models here.
+now = timezone.now()
+
+
+class Users(models.Model):
+    user_fname = models.CharField(max_length=255, verbose_name="First Name")
+    user_lname = models.CharField(max_length=255, verbose_name="Last Name")
+    user_email = models.EmailField(unique=True, max_length=255, verbose_name="Email")
+    user_position = models.CharField(max_length=255, verbose_name="Position")
+    pub_date = models.DateField(default=now)
+
+    def __str__(self) -> str:
+        return self.user_email
