8d3d30df3b80d26063e0613eab6dfbdc905704e2
M-Taha95echo <msoudi2013@gmail.com>
2023-12-08 08:40:05 +0300

    Admin Settings & Search Fields

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Users
 
-# Register your models here.
+admin.site.site_header = "Mohammed Taha"
+admin.site.site_title = "Admin Area!"
+admin.site.index_title = "Welcome to the profiler Admin"
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["user_fname", "user_lname", "user_email", "user_position"]
+    search_fields = ["user_fname", "user_lname", "user_email", "user_position"]
+
+admin.site.register(Users, UserAdmin)
